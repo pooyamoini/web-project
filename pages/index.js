cf87daa6e06582c775e101c717709090c0aca7fc
pooyamoini <pooyamoini2000@gmail.com>
2019-12-21 22:57:48 +0330

    finally add semantic

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
+import 'semantic-ui-css/semantic.min.css'
+
+const Home = () => (
+  <div>
+    <p>hello world</p>
+  </div>
+);
+
+export default Home;
