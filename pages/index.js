9ea28643139ea59bff24ae43291f93153de7e43d
pooyamoini <pooyamoini2000@gmail.com>
2019-12-22 00:06:28 +0330

    very near to stable version

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 import React from "react";
-import Head from "next/head";
-import Nav from "../components/nav";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
+import styled from "styled-components";
 
-const Home = () => (
-  <div>
-    <p>hello world</p>
-  </div>
-);
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const Home = () => {
+  return (
+    <Wrapper>
+      <Title>Hello World!</Title>
+    </Wrapper>
+  );
+};
 
 export default Home;
