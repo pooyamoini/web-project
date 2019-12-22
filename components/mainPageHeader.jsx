bba390562a0b9e07756256b700937c0a33541227
alirezamhm <alireza.am1379@gmail.com>
2019-12-22 22:56:30 +0330

    Create mainPageHeader.jsx

---
+++ components/mainPageHeader.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import React, { Component } from "react";
+import { Grid, GridColumn, Card, Image, Icon,  } from 'semantic-ui-react'
+
+
+export default class Header extends Component {
+   
+    getCard(type, title, date, votes){
+        return(
+            <Card>
+                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>{type}</Card.Header>
+                    <Card.Meta>{title}</Card.Meta>
+                    <Card.Description>{date}</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <p>
+                        <Icon name='thumbs up' />
+                        {votes} Votes
+                    </p>
+                </Card.Content>
+            </Card>
+        )
+    }
+
+    render() {
+        return (
+            <Grid className='Grid' columns={4} divided>
+                <GridColumn>
+                    {this.getCard('Hot', this.props.data.Hot.Title, this.props.data.Hot.Date, this.props.data.Hot.Votes)}
+                </GridColumn>
+                <GridColumn>
+                   {this.getCard('New', this.props.data.New.Title, this.props.data.New.Date, this.props.data.New.Votes)}
+                </GridColumn>
+                <GridColumn>
+                    {this.getCard('Followd', this.props.data.Followed.Title, this.props.data.Followed.Date, this.props.data.Followed.Votes)}
+                </GridColumn>
+                <GridColumn>
+                    {this.getCard('Your Interest', this.props.data.YourInterest.Title, this.props.data.YourInterest.Date, this.props.data.YourInterest.Votes)}            
+                </GridColumn>
+            </Grid>
+        )
+    }
+}
