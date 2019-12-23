d055d7e974c5e6931a5847235ad1e2e102d16a5c
alirezamhm <alireza.am1379@gmail.com>
2019-12-23 18:42:36 +0330

    Grid css added

--- components/mainPageHeader.jsx
+++ components/mainPageHeader.jsx
@@ -1,45 +1,71 @@
 import styled from "styled-components";
 import React, { Component } from "react";
-import { Grid, GridColumn, Card, Image, Icon,  } from 'semantic-ui-react'
+import { Grid as Gr, GridColumn , Card, Image, Icon } from "semantic-ui-react";
 
-
+const Grid = styled(Gr)`
+  width: 80%;
+  margin: 30px auto !important;
+`;
 export default class Header extends Component {
-   
-    getCard(type, title, date, votes){
-        return(
-            <Card>
-                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-                <Card.Content>
-                    <Card.Header>{type}</Card.Header>
-                    <Card.Meta>{title}</Card.Meta>
-                    <Card.Description>{date}</Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <p>
-                        <Icon name='thumbs up' />
-                        {votes} Votes
-                    </p>
-                </Card.Content>
-            </Card>
-        )
-    }
+  getCard(type, title, date, votes) {
+    return (
+      <Card>
+        <Image
+          src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>{type}</Card.Header>
+          <Card.Meta>{title}</Card.Meta>
+          <Card.Description>{date}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <p>
+            <Icon name="thumbs up" />
+            {votes} Votes
+          </p>
+        </Card.Content>
+      </Card>
+    );
+  }
 
-    render() {
-        return (
-            <Grid className='Grid' columns={4} divided>
-                <GridColumn>
-                    {this.getCard('Hot', this.props.data.Hot.Title, this.props.data.Hot.Date, this.props.data.Hot.Votes)}
-                </GridColumn>
-                <GridColumn>
-                   {this.getCard('New', this.props.data.New.Title, this.props.data.New.Date, this.props.data.New.Votes)}
-                </GridColumn>
-                <GridColumn>
-                    {this.getCard('Followd', this.props.data.Followed.Title, this.props.data.Followed.Date, this.props.data.Followed.Votes)}
-                </GridColumn>
-                <GridColumn>
-                    {this.getCard('Your Interest', this.props.data.YourInterest.Title, this.props.data.YourInterest.Date, this.props.data.YourInterest.Votes)}            
-                </GridColumn>
-            </Grid>
-        )
-    }
+  render() {
+    return (
+      <Grid id="Grid" columns={4} divided>
+        <GridColumn>
+          {this.getCard(
+            "Hot",
+            this.props.data.Hot.Title,
+            this.props.data.Hot.Date,
+            this.props.data.Hot.Votes
+          )}
+        </GridColumn>
+        <GridColumn>
+          {this.getCard(
+            "New",
+            this.props.data.New.Title,
+            this.props.data.New.Date,
+            this.props.data.New.Votes
+          )}
+        </GridColumn>
+        <GridColumn>
+          {this.getCard(
+            "Followed",
+            this.props.data.Followed.Title,
+            this.props.data.Followed.Date,
+            this.props.data.Followed.Votes
+          )}
+        </GridColumn>
+        <GridColumn>
+          {this.getCard(
+            "Your Interest",
+            this.props.data.YourInterest.Title,
+            this.props.data.YourInterest.Date,
+            this.props.data.YourInterest.Votes
+          )}
+        </GridColumn>
+      </Grid>
+    );
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import styled from "styled-components";
 import { Icon } from "semantic-ui-react";
-
+import Header from '../components/mainPageHeader';
+import headerSample from '../public/headerSample';
 // Create a Title component that'll render an <h1> tag with some styles
 const Title = styled.h1`
   font-size: 1.5em;
@@ -17,9 +18,7 @@ const Wrapper = styled.section`
 
 const Home = () => {
   return (
-    <Wrapper>
-      <Title>Hello World!</Title>
-    </Wrapper>
+    <Header data={headerSample}></Header>
   );
 };
 
