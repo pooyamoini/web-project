75ca1e53af8dba1f889ef6992480777c6c56d1aa
pooyamoini <pooyamoini2000@gmail.com>
2019-12-26 10:36:53 +0330

    little refactor

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import "semantic-ui-css/semantic.min.css";
 import Navbar from "../components/global/largeNavbar";
 import GloBalStyle from "../components/global/globalStyle";
 import MainContainer from "../components/home-page/index";
-import Header from "../components/mainPageHeader";
+import Header from "../components/home-page-contents/mainPageHeader";
 import HeaderData from "../public/headerSample";
 
 const Home = () => {
