6d002572facf7f56db5f2299e15da41a5b2e37e7
pooyamoini <pooyamoini2000@gmail.com>
2019-12-30 11:38:07 +0330

    more improve in post-mobile

--- components/home-page/post-mobile.jsx
+++ components/home-page/post-mobile.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, Segment, Image, Card } from 'semantic-ui-react'
+import { Grid, Segment, Image, Card, Container } from 'semantic-ui-react'
 import styled from 'styled-components'
 
 const Name = styled.span`
@@ -8,30 +8,36 @@ const Name = styled.span`
   color: white;
 `
 
+const Meta = styled.span`
+  float: right;
+  margin-left: auto;
+  margin-right: 3rem;
+  color: white;
+`
+
 class Post extends Component {
+  constructor (props) {
+    super(props)
+  }
+
   render () {
     return (
       <Grid>
         <Grid.Row textAlign='left' style={{ padding: '0' }}>
-          <Segment basic style={{ padding: '0', marginLeft: '2rem' }}>
-            <Image src='/static/Images/global/avatar.jpg' avatar />
-            <Name>pooya moini</Name>
+          <Segment
+            basic
+            style={{ padding: '0', marginLeft: '2rem', width: '100%' }}
+          >
+            <Image src={this.props.src} avatar />
+            <Name>{this.props.name}</Name>
+            <Meta>30 mins ago</Meta>
           </Segment>
         </Grid.Row>
         <Grid.Row centered>
           <Card style={{ width: '90vw', padding: '0 !important' }}>
             <Image src='/static/Images/global/avatar1.jpg' wrapped ui={false} />
             <Card.Content>
-              <Card.Header>Matthew</Card.Header>
-              <Card.Meta>
-                <span className='date'>Joined in 2015</span>
-              </Card.Meta>
-              <Card.Description>
-                Matthew is a musician living in Nashville.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>22 Friends</a>
+              <Card.Description>{this.props.desc}</Card.Description>
             </Card.Content>
           </Card>
         </Grid.Row>
