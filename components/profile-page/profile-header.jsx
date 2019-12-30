ce73b53f2f2a5e95988a253164c6c00e67f10844
alirezamhm <alireza.am1379@gmail.com>
2019-12-30 22:27:08 +0330

    profile setting icon sent to right

--- components/profile-page/profile-header.jsx
+++ components/profile-page/profile-header.jsx
@@ -37,7 +37,7 @@ export default class ProfileHeader extends Component {
           <SegmentGroup>
             <SegmentGroup horizontal>
               <Segment>{this.props.data.userName}</Segment>
-              <Segment>
+              <Segment textAlign='center'>
                 <SettingsIcon centered fontSize="large"></SettingsIcon>
               </Segment>
             </SegmentGroup>
