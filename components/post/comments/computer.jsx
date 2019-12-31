b3356ddc68068510317c62c63d7515fc903694d8
pooyamoini <pooyamoini2000@gmail.com>
2019-12-31 18:44:00 +0330

    implement commits very successfully :))))))))))

--- components/post/comments/computer.jsx
+++ components/post/comments/computer.jsx
@@ -1,75 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Comment, Form, Header } from 'semantic-ui-react'
 import Theme from '../../../public/theme'
+import Samples from '../../../public/comments'
 
 const globalStyle = {
   color: Theme.post.textColor
 }
 
-const CommentsPage = ({ display }) => (
-  <Comment.Group style={{ paddingLeft: '1rem !important', display }}>
-    <Header as='h3' dividing color='grey'>
-      Comments
-    </Header>
-    <Comment>
-      <Comment.Avatar src='/static/Images/global/avatar2.jpg' />
-      <Comment.Content>
-        <Comment.Author as='a' style={globalStyle}>
-          Matt
-        </Comment.Author>
-        <Comment.Metadata style={globalStyle}>
-          <div>Today at 5:42PM</div>
-        </Comment.Metadata>
-        <Comment.Text style={globalStyle}>How artistic!</Comment.Text>
-        <Comment.Actions>
-          <Comment.Action style={globalStyle}>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
-
-    <Comment>
-      <Comment.Avatar src='/static/Images/global/avatar1.jpg' />
-      <Comment.Content>
-        <Comment.Author as='a' style={globalStyle}>
-          Elliot Fu
-        </Comment.Author>
-        <Comment.Metadata style={globalStyle}>
-          <div>Yesterday at 12:30AM</div>
-        </Comment.Metadata>
-        <Comment.Text style={globalStyle}>
-          <p>This has been very useful for my research. Thanks as well!</p>
-        </Comment.Text>
-        <Comment.Actions>
-          <Comment.Action style={globalStyle}>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
+const CommentComp = ({ src, name, time, content, reply }) => {
+  let Replies = () => <div />
+  if (reply !== undefined)
+    Replies = () => (
       <Comment.Group>
-        <Comment>
-          <Comment.Avatar src='/static/Images/global/avatar.jpg' />
-          <Comment.Content>
-            <Comment.Author as='a' style={globalStyle}>
-              Jenny Hess
-            </Comment.Author>
-            <Comment.Metadata style={globalStyle}>
-              <div>Just now</div>
-            </Comment.Metadata>
-            <Comment.Text style={globalStyle}>
-              Elliot you are always so right
-            </Comment.Text>
-            <Comment.Actions style={globalStyle}>
-              <Comment.Action style={globalStyle}>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
+        {reply.map(x => (
+          <CommentComp {...x} key={x} />
+        ))}
       </Comment.Group>
+    )
+
+  return (
+    <Comment>
+      <Comment.Avatar src={src} />
+      <Comment.Content>
+        <Comment.Author as='a' style={globalStyle}>
+          {name}
+        </Comment.Author>
+        <Comment.Metadata style={globalStyle}>
+          <div>{time}</div>
+        </Comment.Metadata>
+        <Comment.Text style={globalStyle}>{content}</Comment.Text>
+        <Comment.Actions>
+          <Comment.Action style={globalStyle}>Reply</Comment.Action>
+        </Comment.Actions>
+      </Comment.Content>
+      <Replies />
     </Comment>
-    <Form reply>
-      <Form.TextArea style={{ opacity: '0.65' }} />
-      <Button secondary positive style={{ marginBottom: '1rem' }}>
-        Add Reply
-      </Button>
-    </Form>
-  </Comment.Group>
-)
+  )
+}
+
+const GenerateComments = ({ data }) =>
+  data.map(x => <CommentComp {...x} key={x.content} />)
+
+class CommentsPage extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { comments: Samples }
+  }
+
+  render () {
+    const { display } = this.props
+    const { comments } = this.state
+    return (
+      <Comment.Group style={{ paddingLeft: '1rem !important', display }}>
+        <Header as='h3' dividing color='grey'>
+          Comments
+        </Header>
+        <GenerateComments data={comments} />
+        <Form reply>
+          <Form.TextArea style={{ opacity: '0.65' }} />
+          <Button secondary positive style={{ marginBottom: '1rem' }}>
+            Add Reply
+          </Button>
+        </Form>
+      </Comment.Group>
+    )
+  }
+}
 
 export default CommentsPage
