e4b21b17abb106af2c3852c88210bfe35683b9a8
pooyamoini <pooyamoini2000@gmail.com>
2020-01-01 00:57:37 +0330

    comments almost done^^

--- components/post/comments/computer.jsx
+++ components/post/comments/computer.jsx
@@ -8,7 +8,7 @@ const globalStyle = {
   color: Theme.post.textColor
 }
 
-const CommentComp = ({ src, name, time, content, reply }) => {
+const CommentComp = ({ src, name, time, content, reply, id, func }) => {
   let Replies = () => <div />
   if (reply !== undefined)
     Replies = () => (
@@ -18,7 +18,7 @@ const CommentComp = ({ src, name, time, content, reply }) => {
         ))}
       </Comment.Group>
     )
-  const props = { name, src, content }
+  const props = { name, src, content, id }
   return (
     <Comment>
       <Comment.Avatar src={src} />
@@ -31,7 +31,7 @@ const CommentComp = ({ src, name, time, content, reply }) => {
         </Comment.Metadata>
         <Comment.Text style={globalStyle}>{content}</Comment.Text>
         <Comment.Actions>
-          <ModalReply Trigger={Comment.Action} {...props} />
+          <ModalReply Trigger={Comment.Action} {...props} handleFunc={func} />
         </Comment.Actions>
       </Comment.Content>
       <Replies />
@@ -39,14 +39,15 @@ const CommentComp = ({ src, name, time, content, reply }) => {
   )
 }
 
-const GenerateComments = ({ data }) =>
-  data.map(x => <CommentComp {...x} key={x.content} />)
+const GenerateComments = ({ data, func }) =>
+  data.map(x => <CommentComp {...x} func={func} key={x.content} />)
 
 class CommentsPage extends Component {
   constructor (props) {
     super(props)
     this.state = { comments: Samples }
     this.addComment = this.addComment.bind(this)
+    this.addReply = this.addReply.bind(this)
   }
 
   addComment () {
@@ -69,6 +70,27 @@ class CommentsPage extends Component {
     this.setState({ comments })
   }
 
+  addReply (id) {
+    const name = 'Luis Suarez'
+    const src = '/static/Images/global/me.jpg'
+    const content = document.getElementById('reply-field').value
+    if (content == '') return
+    document.getElementById('reply-field').value = ''
+    let time = new Date()
+    const { comments } = this.state
+    time = time
+      .getHours()
+      .toString()
+      .concat(' hours ago')
+    comments[comments.map(x => x.id).indexOf(id)]['reply'].push({
+      name,
+      src,
+      content,
+      time
+    })
+    this.setState({ comments })
+  }
+
   render () {
     const { display } = this.props
     const { comments } = this.state
@@ -77,7 +99,7 @@ class CommentsPage extends Component {
         <Header as='h3' dividing color='grey'>
           Comments
         </Header>
-        <GenerateComments data={comments} />
+        <GenerateComments func={this.addReply} data={comments} />
         <Form reply onSubmit={this.addComment}>
           <Form.TextArea
             style={{ opacity: '0.65' }}
--- components/post/reply-computer.jsx
+++ components/post/reply-computer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   Button,
   Header,
@@ -15,38 +15,76 @@ const globalStyle = {
   color: Theme.post.textColor
 }
 
-const ModalReply = ({ Trigger, name, content, src }) => (
-  <Modal
-    trigger={<Trigger style={globalStyle}>reply</Trigger>}
-    basic
-    size='small'
-  >
-    <Header content='Add Comment' />
-    <Divider />
-    <Modal.Content>
-      <Comment.Group>
-        <Comment>
-          <Comment.Content>
-            <Image src={src} size='mini' />
-            <Comment.Author as='a' style={globalStyle}>
-              {name}
-            </Comment.Author>
-            <Comment.Text style={globalStyle}>{content}</Comment.Text>
-          </Comment.Content>
-        </Comment>
-      </Comment.Group>
-      <Form reply>
-        <Form.TextArea
-          style={{ opacity: '0.65' }}
-          name='textfield'
-          id='comtextfield'
-        />
-        <Button secondary positive style={{ marginBottom: '1rem' }}>
-          Add Reply
-        </Button>
-      </Form>
-    </Modal.Content>
-  </Modal>
-)
+class ModalReply extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { open: false }
+    this.handleOpen = this.handleOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleOpen () {
+    this.setState({ open: true })
+  }
+
+  handleClose () {
+    this.setState({ open: false })
+  }
+
+  handleSubmit () {
+    this.props.handleFunc(this.props.id);
+    this.handleClose()
+  }
+
+  render () {
+    const { Trigger, name, content, src } = this.props
+    const { open } = this.state
+    return (
+      <Modal
+        trigger={
+          <Trigger style={globalStyle} onClick={this.handleOpen}>
+            reply
+          </Trigger>
+        }
+        basic
+        size='small'
+        open={open}
+        onClose={this.handleClose}
+      >
+        <Header content='Add Comment' />
+        <Divider />
+        <Modal.Content>
+          <Comment.Group>
+            <Comment>
+              <Comment.Content>
+                <Image src={src} size='mini' />
+                <Comment.Author as='a' style={globalStyle}>
+                  {name}
+                </Comment.Author>
+                <Comment.Text style={globalStyle}>{content}</Comment.Text>
+              </Comment.Content>
+            </Comment>
+          </Comment.Group>
+          <Form reply>
+            <Form.TextArea
+              style={{ opacity: '0.65' }}
+              name='textfield'
+              id='reply-field'
+            />
+            <Button
+              secondary
+              positive
+              style={{ marginBottom: '1rem' }}
+              onClick={this.handleSubmit}
+            >
+              Add Reply
+            </Button>
+          </Form>
+        </Modal.Content>
+      </Modal>
+    )
+  }
+}
 
 export default ModalReply
