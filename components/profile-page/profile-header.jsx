fea8e181d04d25976f73586a501e4dd9bd02155b
alirezamhm <alireza.am1379@gmail.com>
2019-12-31 14:51:46 +0330

    new post button and modal added

--- components/profile-page/profile-header.jsx
+++ components/profile-page/profile-header.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import Theme from "../../public/theme";
 import SettingsIcon from "@material-ui/icons/Settings";
-
+import IconButton from "@material-ui/core/IconButton";
 import { Grid as Gr, Segment as Seg, Card, Image } from "semantic-ui-react";
 
 const Grid = styled(Gr)`
@@ -12,14 +12,14 @@ const Grid = styled(Gr)`
 `;
 
 const Segment = styled(Seg)`
-  border: none !important;
+  // border: none !important;
   color: white;
   background: transparent !important;
 `;
 
 const SegmentGroup = styled(Segment.Group)`
-  border: none !important;
-  box-shadow: none !important;
+  // border: none !important;
+  // box-shadow: none !important;
 `;
 
 export default class ProfileHeader extends Component {
@@ -37,9 +37,15 @@ export default class ProfileHeader extends Component {
         <Grid.Column width={7}>
           <SegmentGroup>
             <SegmentGroup horizontal>
-              <Segment>{this.props.data.userName}</Segment>
-              <Segment textAlign='center'>
-                <SettingsIcon centered fontSize="large"></SettingsIcon>
+              <Segment style={{
+                'margin-top': '1rem'
+              }}>{this.props.data.userName}</Segment>
+              <Segment textAlign="center">
+                <IconButton aria-label="setting" size='medium' style={{
+                  color: 'white',
+                }}>
+                  <SettingsIcon centered fontSize="large"></SettingsIcon>
+                </IconButton>
               </Segment>
             </SegmentGroup>
             <SegmentGroup horizontal>
--- components/profile-page/profile-posts.jsx
+++ components/profile-page/profile-posts.jsx
@@ -1,47 +1,125 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import styled from "styled-components";
-import {Menu as Me, Container} from 'semantic-ui-react'
+import {
+  Menu as Me,
+  Container,
+  Button,
+  Header,
+  Modal
+} from "semantic-ui-react";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
 
 const Menu = styled(Me)`
-    color: white !important;
-    border-top: none !important;
-    border-right: none !important;
-    border-left: none !important;
-    border-color: rgb(102, 102, 102) !important;
+  color: white !important;
+  border-top: none !important;
+  border-right: none !important;
+  border-left: none !important;
+  border-color: rgb(102, 102, 102) !important;
 `;
 
-const MenuItem = styled(Menu.Item)`
-`;
+const MenuItem = styled(Menu.Item)``;
+
+class NewPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { modalOpen: false };
+  }
+
+  handleClose = () => this.setState({ modalOpen: false });
+
+  handleOpen = () => this.setState({ modalOpen: true });
+
+  render() {
+    return (
+      <Modal
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        trigger={
+          <Button
+            icon
+            circular
+            onClick = {this.handleOpen}
+            size="small"
+            style={{
+              "background-color": "white",
+              color: "black",
+              "margin-right": "2rem",
+              visibility: this.props.active
+            }}
+          >
+            <AddIcon size="large" />
+          </Button>
+        }
+        basic
+        size="small"
+      >
+        <Header content="Create New Post" />
+        <Modal.Content>
+          <p>
+            Your inbox is getting full, would you like us to enable automatic
+            archiving of old messages?
+          </p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button basic color="red" inverted onClick={this.handleClose}>
+            Cancel
+          </Button>
+          <Button color="green" inverted onClick={this.handleClose}>
+            Done
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    );
+  }
+}
 
 export default class ProfilePosts extends Component {
-    constructor(props){
-        super(props);
-        this.state = {activeSection: 'your posts'}
-    }
+  constructor(props) {
+    super(props);
+    this.state = { activeSection: "your posts" };
+  }
 
-    handleItemClick = (e, { name }) => this.setState({ activeSection: name })
+  handleItemClick = (e, { name }) => this.setState({ activeSection: name });
 
-    render() {
-        const { activeSection } = this.state
+  render() {
+    const { activeSection } = this.state;
 
-        return (
-            <Container textAlign = 'center' style={{
-                width: '70%;',
-                "margin": '5px auto !important;'
-            }}> 
-                <Menu inverted borderless pointing secondary compact width={2} size='massive' >                        
-                    <MenuItem
-                        name='your posts'
-                        active={activeSection == 'your posts'}
-                        onClick={this.handleItemClick}
-                    />
-                    <MenuItem
-                        name='followed posts'
-                        active={activeSection == 'followed posts'}      
-                        onClick={this.handleItemClick}
-                    />
-                </Menu>
-            </Container>
-        )
-    }
+    return (
+      <Container
+        textAlign="center"
+        style={{
+          width: "70%;",
+          margin: "1rem auto !important;"
+        }}
+      >
+        <NewPost
+          active={activeSection == "your posts" ? "visible" : "hidden"}
+        />
+        <Menu
+          inverted
+          borderless
+          pointing
+          secondary
+          compact
+          width={2}
+          size="massive"
+          style={{
+            "margin-top": "-1.7rem"
+          }}
+        >
+          <MenuItem
+            name="your posts"
+            active={activeSection == "your posts"}
+            onClick={this.handleItemClick}
+          />
+          <MenuItem
+            name="followed posts"
+            active={activeSection == "followed posts"}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+      </Container>
+    );
+  }
 }
