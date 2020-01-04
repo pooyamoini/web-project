8c3a05f3f3ad99e1eea93f60b786646873ad789f
pooyamoini <pooyamoini2000@gmail.com>
2020-01-04 20:55:34 +0330

    add reply

--- components/post/comments/comment.jsx
+++ components/post/comments/comment.jsx
@@ -2,48 +2,66 @@ import React, { Component } from 'react'
 import { Comment, Form, Button } from 'semantic-ui-react'
 import Theme from '../../../public/theme'
 
+const Reply = ({ display }) => (
+  <Form reply style={{ display }}>
+    <Form.TextArea style={{ marginLeft: '5%', width: '90%', height: '20%' }} />
+    <Button
+      secondary
+      positive
+      style={{ marginBottom: '1rem', marginLeft: '5%' }}
+    >
+      add Reply
+    </Button>
+  </Form>
+)
+
 class CommentComp extends Component {
   constructor (props) {
     super(props)
-    this.state = { displayReply: 'none', open: false }
+    this.state = { display: 'none' }
     this.toReply = this.toReply.bind(this)
     this.doneReply = this.doneReply.bind(this)
   }
 
   doneReply () {
-    // post data
-    this.setState({ displayReply: 'none' })
+    this.setState({ display: 'none' })
   }
 
   toReply () {
-    const { displayReply } = this.state
-    if (displayReply === 'block') {
-      this.setState({ displayReply: 'none' })
+    const { display } = this.state
+    if (display === 'block') {
+      this.setState({ display: 'none' })
       return
     }
-    this.setState({ displayReply: 'block' })
+    this.setState({ display: 'block' })
   }
 
   render () {
     const { name, date, content, src } = this.props
     const color = Theme.post.textColor
+    const { display } = this.state
     const gStyle = { color }
     return (
-      <Comment style={{ marginLeft: '1rem' }}>
-        <Comment.Avatar src={src} />
-        <Comment.Content>
-          <Comment.Author style={gStyle} as='a'>
-            {name}
-          </Comment.Author>
-          <Comment.Metadata style={{color: 'grey'}}>
-            <div>{date}</div>
-          </Comment.Metadata>
-          <Comment.Text style={gStyle}>{content}</Comment.Text>
-          <Comment.Actions>
-            <Comment.Action style={{color: 'grey'}}>Reply</Comment.Action>
-          </Comment.Actions>
-        </Comment.Content>
-      </Comment>
+      <>
+        <Comment style={{ marginLeft: '1rem' }}>
+          <Comment.Avatar src={src} />
+          <Comment.Content>
+            <Comment.Author style={gStyle} as='a'>
+              {name}
+            </Comment.Author>
+            <Comment.Metadata style={{ color: 'grey' }}>
+              <div>{date}</div>
+            </Comment.Metadata>
+            <Comment.Text style={gStyle}>{content}</Comment.Text>
+            <Comment.Actions>
+              <Comment.Action style={{ color: 'grey' }} onClick={this.toReply}>
+                Reply
+              </Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+          <Reply display={display} />
+        </Comment>
+      </>
     )
   }
 }
