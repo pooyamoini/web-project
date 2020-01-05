49be0673859de1f21ca59a2336393e7ae6d0540e
alirezamhm <alireza.am1379@gmail.com>
2020-01-05 16:21:30 +0330

    index.js changed

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,18 @@
-import React from "react";
-import "semantic-ui-css/semantic.min.css";
-import Navbar from "../components/global/navbar-index";
-import GloBalStyle from "../components/global/globalStyle";
-import MainContainer from "../components/home-page/index";
-import Header from "../components/home-page-contents/mainPageHeader";
-import HeaderData from "../public/headerSample";
-import ProfileHeader from '../components/profile-page/profile-header'
-import ProfileHeaderMobile from '../components/profile-page/profile-header-mobile'
-import ProfileData from "../public/json-files/profile"
-import ProfilePostsContainer from "../components/profile-page/profile-posts-container"
-import EditProfileDesktop from '../components/profile-page/edit-profile-desktop'
-import EditProfileMobile from '../components/profile-page/edit-profile-mobile'
-import EditProfile from '../components/profile-page/edit-profile'
+import React from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import Navbar from '../components/global/navbar-index'
+import GloBalStyle from '../components/global/globalStyle'
+import MainContainer from '../components/home-page/index'
+import Header from '../components/home-page-contents/home-header'
+import HeaderData from '../public/headerSample'
+
 const Home = () => {
   return (
     <>
       <GloBalStyle />
       <Navbar />
-      {/* <ProfileHeader type = 'other'></ProfileHeader>
-      <ProfilePostsContainer data = {ProfileData} type = 'other'></ProfilePostsContainer> */}
-      <EditProfile data = {ProfileData}></EditProfile>   
+      <Header data={HeaderData} />
+      <MainContainer />
     </>
   )
 }
