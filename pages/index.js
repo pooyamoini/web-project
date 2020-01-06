4e6fd12d2a5388494a756c0ebfa4942a5bab8f73
alirezamhm <alireza.am1379@gmail.com>
2020-01-06 12:54:20 +0330

    posting method changed

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import 'semantic-ui-css/semantic.min.css'
-import Navbar from '../components/global/navbar-index'
-import GloBalStyle from '../components/global/globalStyle'
-import MainContainer from '../components/home-page/index'
-import Header from '../components/home-page-contents/home-header'
-import HeaderData from '../public/headerSample'
-
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import Navbar from "../components/global/navbar-index";
+import GloBalStyle from "../components/global/globalStyle";
+import ProfileHeader from '../components/profile-page/profile-header'
+import ProfileData from "../public/json-files/profile"
+import ProfilePostsContainer from "../components/profile-page/profile-posts-container"
 const Home = () => {
   return (
     <>
       <GloBalStyle />
       <Navbar />
-      <Header data={HeaderData} />
-      <MainContainer />
+      <ProfileHeader type = 'self'></ProfileHeader>
+      <ProfilePostsContainer data = {ProfileData} type = 'self'></ProfilePostsContainer> 
     </>
   )
 }
