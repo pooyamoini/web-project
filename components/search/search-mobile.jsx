575d8e72864b8f061c418301ea65bf300d71c0ba
alirezamhm <alireza.am1379@gmail.com>
2020-02-02 22:27:30 +0330

    search page responsived

--- components/search/search-mobile.jsx
+++ components/search/search-mobile.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import { Segment as Seg, Image, Divider } from "semantic-ui-react";
+import {
+  Segment as Seg,
+  Image,
+  Divider,
+  Container,
+  Menu
+} from "semantic-ui-react";
 import Post from "../profile-page/profile-post";
 
 const Segment = styled(Seg)`
@@ -9,118 +15,150 @@ const Segment = styled(Seg)`
 `;
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeSection: "accounts" };
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeSection: name });
+
+  getResults() {
+    const { activeSection } = this.state;
+
+    if (activeSection == "accounts") {
+      return (
+        <>
+          <Segment>
+            <Image
+              size="tiny"
+              circular
+              centered
+              src="static/Images/profiles/me.jpg"
+            />
+          </Segment>
+          <Segment inverted style={{ borderBottom: "solid 0.5px darkgray" }}>
+            Alireza Mohammadian
+          </Segment>
+
+          <Segment>
+            <Image
+              size="tiny"
+              circular
+              centered
+              src="static/Images/profiles/me.jpg"
+            />
+          </Segment>
+          <Segment inverted style={{ borderBottom: "solid 0.5px darkgray" }}>
+            Alireza Mohammadian
+          </Segment>
+        </>
+      );
+    }
+
+    if (activeSection == "posts") {
+      return (
+        <>
+          <Segment
+            style={{ width: "80%", margin: "0 auto" }}
+            textAlign="center"
+          >
+            <Post
+              src="static/Images/profiles/avatar0.jpg"
+              name="kain"
+              date="3 mins ago"
+              id="5"
+              image="static/Images/photos/test10.png"
+              desc="da da da la ba ba ba"
+            ></Post>
+          </Segment>
+
+          <Segment
+            style={{ width: "80%", margin: "0 auto" }}
+            textAlign="center"
+          >
+            <Post
+              src="static/Images/profiles/avatar0.jpg"
+              name="kain"
+              date="3 mins ago"
+              id="5"
+              image="static/Images/photos/p7.png"
+              desc="da da da la ba ba ba"
+            ></Post>
+          </Segment>
+        </>
+      );
+    }
+
+    if (activeSection == "channels") {
+      return (
+        <>
+          <Segment>
+            <Image
+              size="tiny"
+              circular
+              centered
+              src="static/Images/profiles/me.jpg"
+            />
+          </Segment>
+          <Segment inverted style={{ borderBottom: "solid 0.5px darkgray" }}>
+            Alireza Mohammadian
+          </Segment>
+
+          <Segment>
+            <Image
+              size="tiny"
+              circular
+              centered
+              src="static/Images/profiles/me.jpg"
+            />
+          </Segment>
+          <Segment inverted style={{ borderBottom: "solid 0.5px darkgray" }}>
+            Alireza Mohammadian
+          </Segment>
+        </>
+      );
+    }
+  }
+
   render() {
     return (
-      <Segment.Group
-        horizontal
+      <Container
+        textAlign="center"
         style={{
-          backgroundColor: "transparent",
-          margin: "3rem auto"
+          width: "70%;"
         }}
       >
-        <Segment
-          textAlign="center"
+        <Menu
+          inverted
+          borderless
+          pointing
+          secondary
+          compact
+          width={3}
+          size="massive"
           style={{
-            borderRight: "solid 1px lightgray",
-            width: "25%"
+            marginTop: "-1.7rem",
+            marginBottom: "2rem"
           }}
         >
-          <Segment inverted>Accounts</Segment>
-          <Segment>
-            <Image
-              size="tiny"
-              circular
-              centered
-              src="static/Images/profiles/me.jpg"
-            />
-          </Segment>
-          <Segment inverted style={{ borderBottom: "solid 0.5px darkgray" }}>
-            Alireza Mohammadian
-          </Segment>
-
-          <Segment>
-            <Image
-              size="tiny"
-              circular
-              centered
-              src="static/Images/profiles/me.jpg"
-            />
-          </Segment>
-          <Segment inverted style={{ borderBottom: "solid 0.5px darkgray" }}>
-            Alireza Mohammadian
-          </Segment>
-        </Segment>
-
-        <Segment
-          textAlign="center"
-          style={{
-            width: "50%",
-            padding: 0
-          }}
-        >
-          <Segment inverted>Posts</Segment>
-          <Segment
-            style={{ width: "70%", margin: "0 auto" }}
-            textAlign="center"
-          >
-            <Post
-              src="static/Images/profiles/avatar0.jpg"
-              name="kain"
-              date="3 mins ago"
-              id="5"
-              image="static/Images/photos/test10.png"
-              desc="da da da la ba ba ba"
-            ></Post>
-          </Segment>
-
-          <Segment
-            style={{ width: "70%", margin: "0 auto" }}
-            textAlign="center"
-          >
-            <Post
-              src="static/Images/profiles/avatar0.jpg"
-              name="kain"
-              date="3 mins ago"
-              id="5"
-              image="static/Images/photos/test10.png"
-              desc="da da da la ba ba ba"
-            ></Post>
-          </Segment>
-        </Segment>
-
-        <Segment
-          textAlign="center"
-          style={{
-            borderLeft: "solid 1px lightgray",
-            width: "25%"
-          }}
-        >
-          <Segment inverted>Channels</Segment>
-          <Segment>
-            <Image
-              size="tiny"
-              circular
-              centered
-              src="static/Images/profiles/me.jpg"
-            />
-          </Segment>
-          <Segment inverted style={{ borderBottom: "solid 0.5px darkgray" }}>
-            Alireza Mohammadian
-          </Segment>
-
-          <Segment>
-            <Image
-              size="tiny"
-              circular
-              centered
-              src="static/Images/profiles/me.jpg"
-            />
-          </Segment>
-          <Segment inverted style={{ borderBottom: "solid 0.5px darkgray" }}>
-            Alireza Mohammadian
-          </Segment>
-        </Segment>
-      </Segment.Group>
+          <Menu.Item
+            name="accounts"
+            active={this.state.activeSection == "accounts"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="posts"
+            active={this.state.activeSection == "posts"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="channels"
+            active={this.state.activeSection == "channels"}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+        {this.getResults()}
+      </Container>
     );
   }
 }
--- pages/search.js
+++ pages/search.js
@@ -2,13 +2,29 @@ import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import Navbar from "../components/global/navbar-index";
 import GloBalStyle from "../components/global/globalStyle";
-import Desktop from '../components/search/search';
+import Computer from '../components/search/search-desktop';
+import Mobile from '../components/search/search-mobile';
+import NoSSR from 'react-no-ssr'
+import { Responsive, Segment } from 'semantic-ui-react'
+
 const Search = () => {
   return (
     <>
       <GloBalStyle />
       <Navbar />
-      <Desktop/>
+      <Segment.Group basic>
+        <NoSSR>
+          <Responsive maxWidth={Responsive.onlyTablet.maxWidth}>
+            <Mobile/>
+          </Responsive>
+          <Responsive
+            minWidth={Responsive.onlyComputer.minWidth}
+            maxWidth={Responsive.onlyComputer.maxWidth}
+          >
+            <Computer/>
+          </Responsive>
+        </NoSSR>
+      </Segment.Group>
     </>
   )
 }
