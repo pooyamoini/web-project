61aef0d127d80690120a9c419c5df520fea13670
alirezamhm <alireza.am1379@gmail.com>
2020-02-03 15:55:03 +0330

    forget passwor dfixed

--- api/account-action/index.jsx
+++ api/account-action/index.jsx
@@ -1,30 +1,31 @@
-import React, { Component } from 'react'
-import Router from 'next/router'
+import React, { Component } from "react";
+import Router from "next/router";
 import {
   SIGNUP_ROUTE,
   LOGIN_ROUTE,
   VALIATE_TOKEN,
-  EDIT_PROFILE
-} from '../urls/account'
-const axios = require('axios')
+  EDIT_PROFILE,
+  FORGET_PASSWORD
+} from "../urls/account";
+const axios = require("axios");
 
 export const signupAPI = params => {
   const res = axios.post(SIGNUP_ROUTE, params, {
     headers: {
-      'Access-Control-Allow-Methods': 'POST'
+      "Access-Control-Allow-Methods": "POST"
     }
-  })
-  return res
-}
+  });
+  return res;
+};
 
 export const loginAPI = params => {
   const res = axios.post(LOGIN_ROUTE, params, {
     headers: {
-      'Access-Control-Allow-Methods': 'POST'
+      "Access-Control-Allow-Methods": "POST"
     }
-  })
-  return res
-}
+  });
+  return res;
+};
 
 export const tokenIsValid = token => {
   const res = axios.post(
@@ -32,23 +33,36 @@ export const tokenIsValid = token => {
     { token },
     {
       headers: {
-        'Access-Control-Allow-Methods': 'POST'
+        "Access-Control-Allow-Methods": "POST"
       }
     }
-  )
-  return res
-}
+  );
+  return res;
+};
 
 export const logout = () => {
-  localStorage.setItem('token', null)
-  Router.push('/')
-}
+  localStorage.setItem("token", null);
+  Router.push("/");
+};
 
 export const editProfileAPI = params => {
   const res = axios.put(EDIT_PROFILE, params, {
     headers: {
-      'Access-Control-Allow-Methods': 'POST'
+      "Access-Control-Allow-Methods": "POST"
     }
-  })
-  return res
-}
+  });
+  return res;
+};
+
+export const forgetPasswordAPI = email => {
+  const res = axios.post(
+    FORGET_PASSWORD,
+    { email },
+    {
+      headers: {
+        "Access-Control-Allow-Methods": "POST"
+      }
+    }
+  );
+  return res;
+};
