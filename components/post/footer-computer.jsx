ffd60f68a0fe121340f394426a8e56d20f0f91ec
alirezamhm <alireza.am1379@gmail.com>
2020-02-05 17:19:56 +0330

    delete  psots modal

--- components/post/footer-computer.jsx
+++ components/post/footer-computer.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import { Menu as Men, Modal } from 'semantic-ui-react'
-import styled from 'styled-components'
-import { deletePostAPI } from '../../api/post/'
-import LikesIcon from '@material-ui/icons/ThumbUpAlt'
-import DisLikeIcon from '@material-ui/icons/ThumbDown'
-import CommentIcon from '@material-ui/icons/ChatBubble'
-import DeleteIcon from '@material-ui/icons/Delete'
-import ShareIcon from '@material-ui/icons/Share'
-import Router from 'next/router'
-import Theme from '../../public/theme'
+import React, { Component } from "react";
+import { Menu as Men, Modal, Button, Header } from "semantic-ui-react";
+import styled from "styled-components";
+import { deletePostAPI } from "../../api/post/";
+import LikesIcon from "@material-ui/icons/ThumbUpAlt";
+import DisLikeIcon from "@material-ui/icons/ThumbDown";
+import CommentIcon from "@material-ui/icons/ChatBubble";
+import DeleteIcon from "@material-ui/icons/Delete";
+import ShareIcon from "@material-ui/icons/Share";
+import Router from "next/router";
+import Theme from "../../public/theme";
 
 const Data = styled.p`
   font-size: 1rem;
@@ -17,63 +17,94 @@ const Data = styled.p`
   margin-right: 1rem !important;
   margin-top: 0.5rem !important;
   display: none;
-`
+`;
 
 const Menu = styled(Men)`
   border: 0 !important;
   box-shadow: none !important;
   margin-top: 0 !important;
-`
+`;
 
 const Icon = styled(Menu.Item)`
   &:before {
     width: 0 !important;
   }
   color: white !important;
-`
+`;
 
 const handleStyle = color => ({
-  color: color === undefined ? 'white' : color
-})
+  color: color === undefined ? "white" : color
+});
 
 class FooterMenu extends Component {
-  constructor (props) {
-    super(props)
-    this.getDeleteIcon = this.getDeleteIcon.bind(this)
-    this.delete = this.delete.bind(this)
+  constructor(props) {
+    super(props);
+    this.getDeleteIcon = this.getDeleteIcon.bind(this);
+    this.delete = this.delete.bind(this);
+    this.state = {deleteModalOpen: false}
   }
 
-  getDeleteIcon () {
-    const { mine } = this.props
+  handleDeleteModalClose = () => {
+    this.setState({deleteModalOpen: false})
+  }
+
+  handleDeleteModalOpen = () => {
+    this.setState({deleteModalOpen: true})
+  }
+
+  getDeleteIcon() {
+    const { mine } = this.props;
     if (mine) {
       return (
-        <Icon>
-          <DeleteIcon
-            onClick={this.delete}
-            fontSize='large'
-            color='error'
-          ></DeleteIcon>
-        </Icon>
-      )
+        <Modal
+          trigger={
+            <Icon>
+              <DeleteIcon
+                fontSize="large"
+                color="error"
+                onClick={this.handleDeleteModalOpen}
+              ></DeleteIcon>
+            </Icon>
+          }
+          open = {this.state.deleteModalOpen}
+          basic
+          size="small"
+        >
+          <Header content="Delete" />
+          <Modal.Content>
+            <p>
+              Are you sure you want to delete this post?
+            </p>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button basic color="red" inverted onClick={this.handleDeleteModalClose}>
+              No
+            </Button>
+            <Button color="green" inverted onClick={this.delete}>
+              Yes
+            </Button>
+          </Modal.Actions>
+        </Modal>
+      );
     }
-    return <></>
+    return <></>;
   }
 
-  async delete () {
-    const { pid } = this.props
-    const token = localStorage.getItem('token')
+  async delete() {
+    const { pid } = this.props;
+    const token = localStorage.getItem("token");
     try {
-      const res = await deletePostAPI(token, pid)
-      Router.push('/profile/profile')
-      return
+      const res = await deletePostAPI(token, pid);
+      Router.push("/profile/profile");
+      return;
     } catch (e) {
-      Router.push('/')
+      Router.push("/");
     }
   }
 
-  render () {
-    const { mine } = this.props
-    const width = mine ? 5 : 4
+  render() {
+    const { mine } = this.props;
+    const width = mine ? 5 : 4;
     return (
       <>
         <Menu
@@ -81,36 +112,36 @@ class FooterMenu extends Component {
           style={{ backgroundColor: Theme.post.backgroundColor }}
           widths={width}
         >
-          <Icon position='left'>
+          <Icon position="left">
             <CommentIcon
-              fontSize='large'
+              fontSize="large"
               style={handleStyle()}
               onClick={this.props.handleCommentClick}
             />
             <Data>12 comments</Data>
           </Icon>
-          <Icon position='left' onClick={this.props.like}>
-            <LikesIcon fontSize='large' style={handleStyle(this.props.color)} />
+          <Icon position="left" onClick={this.props.like}>
+            <LikesIcon fontSize="large" style={handleStyle(this.props.color)} />
             <Data>{this.props.likes} Likes</Data>
           </Icon>
-          <Icon position='left' onClick={this.props.dislike}>
+          <Icon position="left" onClick={this.props.dislike}>
             <DisLikeIcon
-              fontSize='large'
+              fontSize="large"
               style={handleStyle(this.props.colorD)}
             />
             <Data>{this.props.dislikes} dislikes</Data>
           </Icon>
           {this.getDeleteIcon()}
-          <Icon position='right'>
+          <Icon position="right">
             <Modal
-              trigger={<ShareIcon fontSize='large' style={handleStyle()} />}
+              trigger={<ShareIcon fontSize="large" style={handleStyle()} />}
             >
               <Modal.Content>
                 <p
                   onClick={() => {
                     navigator.clipboard.writeText(
                       `localhost:3000/post/${this.props.pid}`
-                    )
+                    );
                   }}
                 >
                   http://www.localhost:3000/post/{this.props.pid}
@@ -123,11 +154,11 @@ class FooterMenu extends Component {
           <Icon name={`${this.props.ncomments}  comments`} />
           <Icon>{this.props.likes} likes</Icon>
           <Icon>{this.props.dislikes} dislikes</Icon>
-          <Icon name='' />
+          <Icon name="" />
         </Menu>
       </>
-    )
+    );
   }
 }
 
-export default FooterMenu
+export default FooterMenu;
